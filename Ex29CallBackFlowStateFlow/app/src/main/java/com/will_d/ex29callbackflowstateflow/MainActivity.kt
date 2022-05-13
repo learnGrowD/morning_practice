6142ba34355134ae3b3becf7e9e9fc86212da616
learnGrowD <dev_willd@naver.com>
2022-05-13 09:13:33 +0900

    commti

---
+++ Ex29CallBackFlowStateFlow/app/src/main/java/com/will_d/ex29callbackflowstateflow/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.will_d.ex29callbackflowstateflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //stateFlow DataHolder의 역할을 하면서 데이터 스트림의 역할까지 한다.
+    }
+}
